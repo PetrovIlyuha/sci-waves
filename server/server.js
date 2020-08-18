2a5ee073b90d2b8d5f0612ef7cbdae5aa15eb200
PetrovIlyuha <dnamix1@gmail.com>
2020-08-18 15:35:52 +0300

    main node server entry point with cors, morgan, connection to MongoDB, and usage of routes branch for authentication

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(morgan("dev"));
+app.use(bodyParser.json({ extended: true }));
+
+mongoose.connect(
+  process.env.MONGO_ACCESS,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  },
+  (err, data) => {
+    if (err) {
+      throw new Error(`DB Access errored with: ${err}`);
+    }
+    console.log(`DB connection established`);
+  }
+);
+
+const authRoutes = require("./routes/auth");
+
+app.use("/api", authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
