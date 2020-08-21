f00cf3029b4a10b498d30162f907c487495e9ec6
PetrovIlyuha <dnamix1@gmail.com>
2020-08-21 17:26:33 +0300

    route for the login process

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { register, registerActivate } = require("../controllers/authController");
-const { userRegistrationValidator } = require("../validators/authValidations");
+const {
+  register,
+  registerActivate,
+  login,
+} = require("../controllers/authController");
+const {
+  userRegistrationValidator,
+  userLoginValidator,
+} = require("../validators/authValidations");
 const { runValidation } = require("../validators");
 
 router.post("/register", userRegistrationValidator, runValidation, register);
 router.post("/register/activate", registerActivate);
+router.post("/login", userLoginValidator, runValidation, login);
 
 module.exports = router;
