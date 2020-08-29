ed030c5f51ca1fb163c55e58e5bea0a0a1d343fd
PetrovIlyuha <dnamix1@gmail.com>
2020-08-29 10:24:49 +0300

    refactoring controller for create category for S3 to operate with base64 images in upload

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -12,50 +12,93 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
 })
 
+// exports.createCategory = (req, res) => {
+//   let form = new formidable.IncomingForm()
+//   form.parse(req, (err, fields, files) => {
+//     if (err) {
+//       return res.status(400).json({
+//         error: "Image could not be uploaded to S3",
+//       })
+//     }
+//     console.log({ err, fields, files })
+//     const { name, content } = fields
+//     const { image } = files
+//     const slug = slugify(name)
+//     let category = new Category({ name, content, slug })
+//     if (image.size > 3000000) {
+//       return res.status(400).json({
+//         error: "Image is too large. Limit is 3Mb",
+//       })
+//     }
+//     // upload image to S3
+//     const params = {
+//       Bucket: "sci-waves",
+//       Key: `category/${uuidv4()}`,
+//       Body: fs.readFileSync(image.path),
+//       ACL: "public-read", // access-control level (in the bucket's policy)
+//       ContentType: `image/jpg`,
+//     }
+//     s3.upload(params, function (err, data) {
+//       if (err) {
+//         console.log(err)
+//         res.status(400).json({ error: "Upload to S3 has failed..." })
+//       }
+//       // console.log("S3 image response after upload", data)
+//       category.image.url = data.Location
+//       category.image.key = data.Key
+//       // save category to DB
+//       category.save((err, success) => {
+//         if (err) res.status(400).json({ error: "Error while saving category" })
+//         return res.json(success)
+//       })
+//     })
+//   })
+// }'
+
 exports.createCategory = (req, res) => {
-  let form = new formidable.IncomingForm()
-  form.parse(req, (err, fields, files) => {
+  const { name, image, content } = req.body
+  // image data
+  const base64Data = new Buffer.from(
+    image.replace(/^data:image\/\w+;base64,/, ""),
+    "base64"
+  )
+  const imageType = image.split(";")[0].split("/")[1]
+  const slug = slugify(name)
+  let category = new Category({ name, content, slug })
+  const params = {
+    Bucket: "sci-waves",
+    Key: `category/${uuidv4()}.${imageType}`,
+    Body: base64Data,
+    ACL: "public-read", // access-control level (in the bucket's policy)
+    ContentEncoding: "base64",
+    ContentType: `image/${imageType}`,
+  }
+  s3.upload(params, function (err, data) {
     if (err) {
-      return res.status(400).json({
-        error: "Image could not be uploaded to S3",
-      })
+      console.log(err)
+      res.status(400).json({ error: "Upload to S3 has failed..." })
     }
-    console.log({ err, fields, files })
-    const { name, content } = fields
-    const { image } = files
-    const slug = slugify(name)
-    let category = new Category({ name, content, slug })
-    if (image.size > 3000000) {
-      return res.status(400).json({
-        error: "Image is too large. Limit is 3Mb",
-      })
-    }
-    // upload image to S3
-    const params = {
-      Bucket: "sci-waves",
-      Key: `category/${uuidv4()}`,
-      Body: fs.readFileSync(image.path),
-      ACL: "public-read", // access-control level (in the bucket's policy)
-      ContentType: `image/jpg`,
-    }
-    s3.upload(params, function (err, data) {
-      if (err) {
-        console.log(err)
-        res.status(400).json({ error: "Upload to S3 has failed..." })
-      }
-      // console.log("S3 image response after upload", data)
-      category.image.url = data.Location
-      category.image.key = data.Key
-      // save category to DB
-      category.save((err, success) => {
-        if (err) res.status(400).json({ error: "Error while saving category" })
-        return res.json(success)
-      })
+    // console.log("S3 image response after upload", data)
+    category.image.url = data.Location
+    category.image.key = data.Key
+    // save category to DB
+    category.save((err, success) => {
+      if (err) res.status(400).json({ error: "Error while saving category" })
+      return res.json(success)
     })
   })
 }
 
-exports.getAllCategories = (req, res) => {}
+exports.getAllCategories = (req, res) => {
+  Category.find({}).exec((err, categories) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Error accessing categories in Database",
+      })
+    }
+    res.json(categories)
+  })
+}
 exports.readSingleCategory = (req, res) => {}
 exports.updateCategory = (req, res) => {}
 exports.removeCategory = (req, res) => {}
