6b95b9656bb13c9ec874a7b9c269ff6cf013411e
PetrovIlyuha <dnamix1@gmail.com>
2020-09-14 13:09:59 +0300

    expanded user controller logic to retrieve user's links and provide it to the front-end

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,24 @@
+const User = require("../models/User")
+const Link = require("../models/Link")
+
 exports.readUserProfile = (req, res) => {
-  req.profile.hashed_password = null
-  req.profile.salt = null
-  return res.json(req.profile)
+  User.findOne({ _id: req.user._id }).exec((err, user) => {
+    if (err) {
+      console.error("Error getting user (customer) from DB: ", err)
+      res.status(401).json(err)
+    }
+    Link.find({ postedBy: user })
+      .populate("categories", "name slug")
+      .populate("postedBy", "name")
+      .sort({ createdAt: -1 })
+      .exec((err, links) => {
+        if (err) {
+          console.error("Error getting links (customer) from DB: ", err)
+          res.status(401).json(err)
+        }
+        user.hashed_password = undefined
+        user.salt = undefined
+        return res.json({ user, links })
+      })
+  })
 }
