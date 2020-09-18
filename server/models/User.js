f40816b77e21939dced68448fb40607eded9eaa2
PetrovIlyuha <dnamix1@gmail.com>
2020-09-18 14:47:36 +0300

    user model modified to include categories of choice

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,7 @@
-const mongoose = require("mongoose");
-const crypto = require("crypto");
+const mongoose = require("mongoose")
+const crypto = require("crypto")
+
+const { ObjectId } = mongoose.Schema
 
 const userSchema = new mongoose.Schema(
   {
@@ -39,39 +41,43 @@ const userSchema = new mongoose.Schema(
       data: String,
       default: "",
     },
+    categories: [
+      {
+        type: ObjectId,
+        ref: "Category",
+        required: true,
+      },
+    ],
   },
   { timestamps: true }
-);
+)
 
 userSchema
   .virtual("password")
   .set(function (password) {
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashed_password = this.encryptPassword(password);
+    this._password = password
+    this.salt = this.makeSalt()
+    this.hashed_password = this.encryptPassword(password)
   })
   .get(function () {
-    return this._password;
-  });
+    return this._password
+  })
 
 userSchema.methods = {
   authenticate: function (plainText) {
-    return this.encryptPassword(plainText) === this.hashed_password;
+    return this.encryptPassword(plainText) === this.hashed_password
   },
   encryptPassword: function (password) {
-    if (!password) return "";
+    if (!password) return ""
     try {
-      return crypto
-        .createHmac("sha1", this.salt)
-        .update(password)
-        .digest("hex");
+      return crypto.createHmac("sha1", this.salt).update(password).digest("hex")
     } catch (err) {
-      return "";
+      return ""
     }
   },
   makeSalt: function () {
-    return Math.round(new Date().valueOf() * Math.random()) + "";
+    return Math.round(new Date().valueOf() * Math.random()) + ""
   },
-};
+}
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema)
